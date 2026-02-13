dbadb1403b92f75592babbfbab7d4b1094f4f136
Bryce Johnson <brycejohn01@gmail.com>
2026-02-12 22:41:19 -0700

    Created app.jsx with baseline code

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div  className="body bg-dark text-light"><header  className="top-header">
+        <h1>BakeOff Board</h1>
+        </header>
+
+        <div  className="app-layout">
+            <nav  className="sidebar">
+                <ul>
+                    <li><a href="index.html">
+                        Home
+                        </a>
+                    </li>
+                    <li><a href="feed.html">
+                        Feed
+                        </a>
+                    </li>
+                    <li><a href="add_recipe.html">
+                        Add Bake
+                        </a>
+                    </li>
+                    <li><a href="cookbook.html">
+                        My Cookbook
+                        </a>
+                    </li>
+                </ul>
+             </nav>
+            
+            <div  className="content">
+                <main  className="home">App components go here</main>
+
+                <footer>
+                <p>Created by Bryce Johnson</p>
+                <p>
+                    <a href="https://github.com/bryjohn01/startup.git">
+                    View on GitHub
+                    </a>
+                </p>
+                </footer>
+            </div>
+        </div>
+    </div>
+    );
 }
