0721c6851cc8228d91850295ad1d30ca470f18fe
Bryce Johnson <brycejohn01@gmail.com>
2026-02-12 23:09:23 -0700

    Added route components in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,47 +2,65 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Feed } from './feed/feed';
+import { Cookbook } from './cookbook/cookbook';
+import { Add_Recipe } from './add_recipe/add_recipe';
+
 export default function App() {
   return (
-    <div  className="body bg-dark text-light"><header  className="top-header">
-        <h1>BakeOff Board</h1>
-        </header>
+    <BrowserRouter>
+        <div  className="body bg-dark text-light"><header  className="top-header">
+            <h1>BakeOff Board</h1>
+            </header>
 
-        <div  className="app-layout">
-            <nav  className="sidebar">
-                <ul>
-                    <li><a href="index.html">
-                        Home
-                        </a>
-                    </li>
-                    <li><a href="feed.html">
-                        Feed
-                        </a>
-                    </li>
-                    <li><a href="add_recipe.html">
-                        Add Bake
-                        </a>
-                    </li>
-                    <li><a href="cookbook.html">
-                        My Cookbook
-                        </a>
-                    </li>
-                </ul>
-             </nav>
-            
-            <div  className="content">
-                <main  className="home">App components go here</main>
+            <div  className="app-layout">
+                <nav  className="sidebar">
+                    <ul>
+                        <li><NavLink to="/">
+                            Home
+                            </NavLink>
+                        </li>
+                        <li><NavLink to="/feed">
+                            Feed
+                            </NavLink>
+                        </li>
+                        <li><NavLink to="/add_recipe">
+                            Add Bake
+                            </NavLink>
+                        </li>
+                        <li><NavLink to="/cookbook">
+                            My Cookbook
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                
+                <div  className="content">
+                    <Routes>
+                        <Route path='/' element={<Login />} />
+                        <Route path='/feed' element={<Feed />} />
+                        <Route path='/add_recipe' element={<Cookbook />} />
+                        <Route path='/cookbook' element={<Add_Recipe />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
 
-                <footer>
-                <p>Created by Bryce Johnson</p>
-                <p>
-                    <a href="https://github.com/bryjohn01/startup.git">
-                    View on GitHub
-                    </a>
-                </p>
-                </footer>
+                    <footer>
+                    <p>Created by Bryce Johnson</p>
+                    <p>
+                        <a href="https://github.com/bryjohn01/startup.git">
+                        View on GitHub
+                        </a>
+                    </p>
+                    </footer>
+                </div>
             </div>
         </div>
-    </div>
+    </BrowserRouter>
     );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
